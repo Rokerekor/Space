41bf6f00938eec59cea4e27d0e4e0fa22b3474fd
Roker <andrewbailey@shaw.ca>
2014-06-12 15:32:26 -0700

    MAP FIXED
    except rotation OOPS

--- Spaace/Assets/Sprites/Map/Map.cs
+++ Spaace/Assets/Sprites/Map/Map.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 
 public class Map : MonoBehaviour {
-	public GameObject map;
 	public GameObject fog;
-	public GameObject playerSprite;
-	public Sprite abandonedSprite;
-	public Sprite storeSprite;
+	public GameObject abandonedSprite;
+	public GameObject storeSprite;
+	public GameObject map;
 
 	GameObject player;
+	GameObject playerSprite;
+
+
 	bool status = false;
 	const int unitsValue = 14;
 	const int spawnSize = 1;
 	const int mapWidth = 100;
 	float unitSize = mapWidth/unitsValue*2;
+	float aspect = 9/16f;
 
 	GameObject[,] fogs = new GameObject[unitsValue,unitsValue];
 	bool[,] explored = new bool[unitsValue,unitsValue];
@@ -24,8 +27,8 @@ public class Map : MonoBehaviour {
 	int timer = 0;
 
 	void Start () {
-		player =  GameObject.FindGameObjectWithTag("Player");;
-		map.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
+		player =  GameObject.FindGameObjectWithTag("Player");
+		playerSprite = map.transform.FindChild("GUIPlayer").gameObject;
 		startFog();
 		startStations();
 	}
@@ -37,50 +40,46 @@ public class Map : MonoBehaviour {
 		}
 	}
 	void startStations(){
-		//stores = new GameObject[GameObject.FindGameObjectsWithTag("store").Length];
-		//stations = new GameObject[GameObject.FindGameObjectsWithTag("abdstation").Length];
 		GameObject[] tempStores = GameObject.FindGameObjectsWithTag("store");
 		GameObject[] tempStations = GameObject.FindGameObjectsWithTag("abdstation");
 		stores = new GameObject[tempStores.Length];
 		stations = new GameObject[tempStations.Length];
 		for(int i=0;i<tempStores.Length;i++){
-			float yVal = Camera.main.camera.orthographicSize;
 			GameObject store = tempStores[i];
-			Vector3 pos = new Vector3(store.transform.position.x/mapWidth * yVal,store.transform.position.y/mapWidth * yVal,0);
-			GameObject newStore = (GameObject)Instantiate(storeSprite,pos,new Quaternion(0,0,0,0));
-			newStore.GetComponent<SpriteRenderer>().sortingOrder = 12;
-			newStore.SetActive(false);
-			newStore.transform.parent = Camera.main.transform;
+			Vector3 position = new Vector3(store.transform.position.x/(mapWidth*2)*aspect + 0.5f,store.transform.position.y/(mapWidth*2) + 0.5f,0.9f);
+			GameObject newStore = (GameObject)Instantiate(storeSprite,new Vector3(0,0,0),new Quaternion(0,0,0,0));
+			newStore.transform.parent = map.transform;
+			newStore.transform.localPosition = position;
+			newStore.transform.localScale =  new Vector3(0.1f*aspect,0.1f,0);
 			stores[i] = newStore;
 		}
 		for(int i=0;i<tempStations.Length;i++){
-			float yVal = Camera.main.camera.orthographicSize;
 			GameObject station = tempStations[i];
-			Vector3 pos = new Vector3(station.transform.position.x/mapWidth * yVal,station.transform.position.y/mapWidth * yVal,0);
-			GameObject newStation = (GameObject)Instantiate(abandonedSprite,pos,new Quaternion(0,0,0,0));
-			newStation.GetComponent<SpriteRenderer>().sortingOrder = 12;
-			newStation.SetActive(false);
-			newStation.transform.parent = Camera.main.transform;
+			Vector3 position = new Vector3(station.transform.position.x/(mapWidth*2)*aspect + 0.5f,station.transform.position.y/(mapWidth*2) + 0.5f,0.9f);
+			GameObject newStation = (GameObject)Instantiate(abandonedSprite,new Vector3(0,0,0),new Quaternion(0,0,0,0));
+			newStation.transform.parent = map.transform;
+			newStation.transform.localPosition = position;
+			newStation.transform.localScale =  new Vector3(0.1f*aspect,0.1f,0);
 			stations[i] = newStation;
 		}
 	}
 	void startFog(){
-		for(int i=(unitsValue/2)-spawnSize;i<(unitsValue/2)+spawnSize;i++){
-			for(int k=(unitsValue/2)-spawnSize;k<(unitsValue/2)+spawnSize;k++){
-				explored[i,k] = true;
-			}
-		}
 		for(int i=0;i<unitsValue;i++){
 			float yVal = Camera.main.camera.orthographicSize * 2 / unitsValue;
 			float xVal = yVal*Camera.main.aspect;
 			for(int k=0;k<unitsValue;k++){
-				Vector3 position = new Vector3(i*yVal - (unitsValue/2-0.5f)*yVal,k*yVal - (unitsValue/2-0.5f)*yVal,1);
+				Vector3 position = new Vector3((float)i/unitsValue*aspect + 0.25f,(float)k/unitsValue + 0.5f/unitsValue,11);
 				position+=Camera.main.transform.position;
-				GameObject newFog = (GameObject)Instantiate(fog,position,map.transform.rotation);
-				newFog.transform.parent = Camera.main.transform;
-				//newFog.transform.localScale =  new Vector3(newFog.transform.localScale.x*20/unitsValue,newFog.transform.localScale.y*20/unitsValue,newFog.transform.localScale.y);
-				newFog.SetActive(false);
+				GameObject newFog = (GameObject)Instantiate(fog,new Vector3(0,0,0),new Quaternion(0,0,0,0));
+				newFog.transform.parent = map.transform;
+				newFog.transform.localPosition = position;
+				newFog.transform.localScale =  new Vector3(1f/unitsValue*aspect,1f/unitsValue,0);
 				fogs[i,k] = newFog;
+			}
+		}
+		for(int i=(unitsValue/2)-spawnSize;i<(unitsValue/2)+spawnSize;i++){
+			for(int k=(unitsValue/2)-spawnSize;k<(unitsValue/2)+spawnSize;k++){
+				Destroy(fogs[i,k]);
 			}
 		}
 	}
@@ -105,72 +104,25 @@ public class Map : MonoBehaviour {
 		if(explored[xVal,yVal] == false){
 			Debug.Log("EXPLORED");
 		}
+		Destroy(fogs[xVal,yVal]);
 		explored[xVal,yVal] = true;
 	}
 	void openMap(){
 		if(!status){
 			status = true;
+
 			Time.timeScale = 0;
 			map.SetActive(true);
-			for(int i=0;i<unitsValue;i++){			
-				for(int k=0;k<unitsValue;k++){
-					GameObject f = fogs[i,k];
-					f.SetActive(!explored[i,k]);
-				}
-			}
-			float yVal = Camera.main.camera.orthographicSize;
-			Vector3 pos = new Vector3(player.transform.position.x/mapWidth * yVal + Camera.main.transform.position.x,player.transform.position.y/mapWidth * yVal + Camera.main.transform.position.y,0);
-			playerSprite.SetActive(true);
-			playerSprite.transform.position = pos;
-			playerSprite.transform.rotation = player.transform.rotation;
 
-			foreach(GameObject store in stores){
-				store.SetActive(true);
-			}
-			foreach(GameObject station in stations){
-				station.SetActive(true);
-			}
-//			foreach(GameObject store in stores){
-//				float hVal = Camera.main.camera.orthographicSize * 2 / unitsValue;
-//				float wVal = hVal*Camera.main.aspect;
-//				int xVal = Mathf.FloorToInt((store.transform.position.x - Camera.main.transform.position.x)/wVal) + unitsValue/2;
-//				int yVal = Mathf.FloorToInt((store.transform.position.y - Camera.main.transform.position.y)/hVal) + unitsValue/2;
-//				Debug.Log("X: " + xVal);
-//				Debug.Log("Y: " + yVal);
-//				if(explored[xVal,yVal]){
-//					store.SetActive(true);
-//				}
-//			}
-//
-//			foreach(GameObject station in stations){
-//				float hVal = Camera.main.camera.orthographicSize * 2 / unitsValue;
-//				float wVal = hVal*Camera.main.aspect;
-//				int xVal = Mathf.FloorToInt((station.transform.position.x - Camera.main.transform.position.x)/wVal) + unitsValue/2;
-//				int yVal = Mathf.FloorToInt((station.transform.position.y - Camera.main.transform.position.y)/hVal) + unitsValue/2;
-//				Debug.Log("X: " + xVal);
-//				Debug.Log("Y: " + yVal);
-//				if(explored[xVal,yVal]){
-//					station.SetActive(true);
-//				}
-//			}
+			playerSprite.transform.position = new Vector3(player.transform.position.x/(mapWidth*2)*aspect + 0.5f,player.transform.position.y/(mapWidth*2) + 0.5f,1);
+			playerSprite.transform.rotation = player.transform.rotation;
 		}
 	}
 	void closeMap(){
-		Time.timeScale = 1;
-		map.SetActive(false);
-		for(int i=0;i<unitsValue;i++){			
-			for(int k=0;k<unitsValue;k++){
-				GameObject f = fogs[i,k];
-				f.SetActive(false);
-			}
+		if(status){
+			status = false;
+			Time.timeScale = 1;
+			map.SetActive(false);
 		}
-		foreach(GameObject store in stores){
-			store.SetActive(false);
-		}
-		foreach(GameObject station in stations){
-			station.SetActive(false);
-		}
-		playerSprite.SetActive(false);
-		status = false;
 	}
 }
